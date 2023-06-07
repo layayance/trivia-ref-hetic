bb38a6a8a375fe6cdf5b5cb4dfd61cd140ced8fe
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2023-06-07 14:12:40 +0200

    Refactor

---
+++ src/Board/index.spec.ts
@@ -0,0 +1,49 @@
+import Board from ".";
+
+test("Pop", () => {
+  expect(Board.computeCurrentCategory(0)).toEqual("Pop");
+});
+
+test("Pop", () => {
+  expect(Board.computeCurrentCategory(4)).toEqual("Pop");
+});
+
+test("Pop", () => {
+  expect(Board.computeCurrentCategory(8)).toEqual("Pop");
+});
+
+test("Science", () => {
+  expect(Board.computeCurrentCategory(1)).toEqual("Science");
+});
+
+test("Science", () => {
+  expect(Board.computeCurrentCategory(5)).toEqual("Science");
+});
+
+test("Science", () => {
+  expect(Board.computeCurrentCategory(9)).toEqual("Science");
+});
+
+test("Sports", () => {
+  expect(Board.computeCurrentCategory(2)).toEqual("Sports");
+});
+
+test("Sports", () => {
+  expect(Board.computeCurrentCategory(6)).toEqual("Sports");
+});
+
+test("Sports", () => {
+  expect(Board.computeCurrentCategory(10)).toEqual("Sports");
+});
+
+test("Rock", () => {
+  expect(Board.computeCurrentCategory(3)).toEqual("Rock");
+});
+
+test("Rock", () => {
+  expect(Board.computeCurrentCategory(7)).toEqual("Rock");
+});
+
+test("Rock", () => {
+  expect(Board.computeCurrentCategory(11)).toEqual("Rock");
+});
